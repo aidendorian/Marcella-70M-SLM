0f2b903ab9295ffd8fdb3047fc4bf6bebb2a0254
aidendorian <resorttoaiden@gmail.com>
2026-01-18 14:35:43 +0000

    corrected tokenizer

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -10,22 +10,12 @@ class Tokenizer:
         self.tokenizer = SentencePieceProcessor()
         self.tokenizer.Load(model_file=tokenizer_model)
         self.num_words = self.tokenizer.vocab_size()
-        self.bos_id = self.tokenizer.bos_id()
-        self.eos_id = self.tokenizer.eos_id()
         self.unk_id = self.tokenizer.unk_id()
         
     def encode(self,
-               sentence:str,
-               add_bos:bool=False,
-               add_eos:bool=False) -> List[int]:
+               sentence:str) -> List[int]:
         
         encoded = self.tokenizer.EncodeAsIds(sentence)
-        if add_bos:
-            encoded = [self.bos_id] + encoded
-            
-        if add_eos:
-            encoded = encoded + [self.eos_id]
-            
         return encoded
     
     def decode(self,
