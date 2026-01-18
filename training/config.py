7844af7172d8064750072339ac9c0dcd0f2dc8cc
mmithra29 <mithrahiranya04@gmail.com>
2026-01-18 20:04:00 +0530

    Added training components

--- training/config.py
+++ training/config.py
@@ -6,3 +6,13 @@ class Config:
         self.num_heads = 12
         self.attn_dropout = 0.0
         self.ffn_dropout = 0.1
+        self.dataset_name = "draco976/wikipedia-bookcorpus"
+        self.dataset_split = 'train'
+        self.tkn_model = 'models/Marcella_vocab_32K.model'
+        self.block_size = 512
+        self.batch_size = 2
+        self.num_workers = 4
+        self.pin_memory = True
+        self.prefetch_factor = 2
+        self.persistent_workers = True
+        self.max_samples = None
--- training/train.py
+++ training/train.py
@@ -2,9 +2,11 @@ import torch
 from bitsandbytes.optim.adamw import AdamW8bit
 from tqdm import tqdm
 from config import Config
+from torch.nn import CrossEntropyLoss
+from torch.amp.autocast_mode import autocast
+from torch.amp.grad_scaler import GradScaler
 import os
-import random
-from datasets import load_dataset
+from dataloader import get_data
 from src.marcella import Marcella
 
 config = Config()
@@ -17,5 +19,19 @@ model = Marcella(vocab_size=config.vocab_size,
                  ffn_dropout=config.ffn_dropout
                  )
 
-optim = AdamW8bit(model.parameters(), lr=1e-4)
+optimizer = AdamW8bit(model.parameters(), lr=1e-4)
 
+data = get_data(dataset_name=config.dataset_name,
+                dataset_split=config.dataset_split,
+                tkn_model=config.tkn_model,
+                block_size=config.block_size,
+                batch_size=config.batch_size,
+                num_workers=config.batch_size,
+                pin_memory=config.pin_memory,
+                prefetch_factor=config.prefetch_factor,
+                persistent_workers=config.persistent_workers,
+                max_samples=config.max_samples)
+
+loss = CrossEntropyLoss()
+
+scaler = GradScaler("cuda")
